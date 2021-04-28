4f3ef1d171abe3682f2159d473b817217456224b
Xavi <xavi@geekshubs.com>
2021-04-28 12:09:07 +0200

    implemented feature

--- Consumer/src/main/java/com/geekshubs/consumer/application/service/KafkaService.java
+++ Consumer/src/main/java/com/geekshubs/consumer/application/service/KafkaService.java
@@ -1,10 +1,13 @@
 package com.geekshubs.consumer.application.service;
 
+import com.geekshubs.consumer.domain.model.User;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 
 @Service
@@ -12,9 +15,16 @@ public class KafkaService {
 
     Logger log = LoggerFactory.getLogger(KafkaService.class);
 
-    @KafkaListener(topics="prueba")
-    public void consume( String message){
-        log.info("Mensaje consumido: " + message);
+    @KafkaListener(topics="prueba",containerFactory = "kafkaListenerContainerFactory")
+    public void consume(List<ConsumerRecord<String, String>> messages){
+
+       /* messages.forEach(
+                valor->log.info(valor.key())
+        );*/
+        for(ConsumerRecord<String, String> message:messages){
+            log.info("Información mensaje -> Offset {} Particion={} key={} Value={}",
+                    message.offset(), message.partition(),message.key(), message.value());
+        }
     }
 
 
