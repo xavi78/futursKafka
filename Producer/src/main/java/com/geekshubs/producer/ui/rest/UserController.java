71e40ae4badf05c2be26f9b0790737681d7a1f02
Xavi <xavi@geekshubs.com>
2021-04-28 11:37:54 +0200

    implemented feature

--- Producer/src/main/java/com/geekshubs/producer/ui/rest/UserController.java
+++ Producer/src/main/java/com/geekshubs/producer/ui/rest/UserController.java
@@ -1,6 +1,8 @@
 package com.geekshubs.producer.ui.rest;
 
 import com.geekshubs.producer.domain.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +17,19 @@ public class UserController {
     @Autowired
     private KafkaTemplate<String, User> kafkaTemplate;
     private static final String TOPIC= "prueba";
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
 
     @GetMapping("/publish/{name}")
     public String get(@PathVariable("name") final String name){
 
-        kafkaTemplate.send(TOPIC,new User(name, "TIC", 12000l));
+        log.info("Start sended messages");
+
+        for(int i = 0 ; i<1000; i++){
+            log.info("Posición -> "+ i );
+            kafkaTemplate.send(TOPIC,new User(name+i, "TIC", 12000l));
+        }
+
         return "Published successfully";
     }
 
